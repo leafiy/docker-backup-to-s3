1fed4f352568b9f76c753186a9c3f628272f3c80
leafiy <t@leafiy.com>
2020-05-06 04:01:56 +0800

    exclude jenkins folder

--- sync.sh
+++ sync.sh
@@ -3,7 +3,7 @@
 set -e
 date=$(date +"%Y%m%d")
 echo "Job started: $(date) backup $DATA_PATH"
-tar -czvf $DATA_PATHbackup_$date.tar.gz "$DATA_PATH"
+tar -czvf $DATA_PATHbackup_$date.tar.gz --exclude=$DATA_PATHjenkins "$DATA_PATH"
 /usr/local/bin/s3cmd sync $PARAMS $DATA_PATHbackup_$date.tar.gz "$S3_PATH"
 rm $DATA_PATHbackup_$date.tar.gz
 echo "Job finished: $(date)"
